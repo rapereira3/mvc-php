91a55796831169503ed467d355261d80f69ce50d
rapereira3 <r.a.pereira3@gmail.com>
2016-06-26 19:54:09 -0300

    Rodapé OK

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Este arquivo não faz nada, apenas inclui os arquivos necessários
+@lang text
+ */
+
+echo "<!DOCTYPE html>
+<html lang=\"pt_BR\">
+  <header>
+
+  </header>
+</html>";
+
+require_once 'config.php';
+
+echo "<!DOCTYPE html>
+<html lang=\"pt_BR\">
+  <footer>
+    <p><strong>Faculdade de Tecnologia de Franca &ldquo;Thomaz Novelino&rdquo; &ndash; Fatec Franca</strong><br />
+      Rua Irênio Grecco, 4580 &ndash; Vila Imperador<br />
+      Franca/SP &ndash; CEP 14405-191<br />
+      Telefone: (16) 3702-3204 &ndash; Fax: 3702-2854<br />
+      <a href=\"maito:fatecfranca@fatecfranca.edu.br\">fatecfranca@fatecfranca.edu.br</a> &ndash;
+      <a href=\"http://fatecfranca.edu.br\">fatecfranca.edu.br</a>
+    </p>
+    <p>&copy; Copyright 2016 &ndash; <a href=\"mailto:r.a.pereira3@gmail.com\"> Roldão - Rogério</a>. Todos os direitos reservados.</p>
+  </footer>
+</html>";
